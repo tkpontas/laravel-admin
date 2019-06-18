0c25f31f042b9e90240f9fc59e6892af4ec1e519
hirossyi73 <it2@hirossyi.net>
2019-06-18 14:49:30 +0900

    modify admin_base_path to admin_url

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -136,7 +136,7 @@ class AuthController extends Controller
                 return $form->model()->password;
             });
 
-        $form->setAction(admin_base_path('auth/setting'));
+        $form->setAction(admin_url('auth/setting'));
 
         $form->ignore(['password_confirmation']);
 
@@ -149,7 +149,7 @@ class AuthController extends Controller
         $form->saved(function () {
             admin_toastr(trans('admin.update_succeeded'));
 
-            return redirect(admin_base_path('auth/setting'));
+            return redirect(admin_url('auth/setting'));
         });
 
         return $form;
--- src/Controllers/MenuController.php
+++ src/Controllers/MenuController.php
@@ -31,7 +31,7 @@ class MenuController extends Controller
 
                 $row->column(6, function (Column $column) {
                     $form = new \Encore\Admin\Widgets\Form();
-                    $form->action(admin_base_path('auth/menu'));
+                    $form->action(admin_url('auth/menu'));
 
                     $menuModel = config('admin.database.menu_model');
                     $permissionModel = config('admin.database.permissions_model');
