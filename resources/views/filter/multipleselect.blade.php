9872e947d536a8d23f6bede6502eede0d96c2647
hsatou <it2@hirossyi.net>
2021-04-27 12:51:22 +0900

    Bug fix if multiple select filter value is array

--- resources/views/filter/multipleselect.blade.php
+++ resources/views/filter/multipleselect.blade.php
@@ -1,6 +1,10 @@
 <select class="form-control {{ $class }}" name="{{$name}}[]" multiple style="width: 100%;">
     <option></option>
     @foreach($options as $select => $option)
-        <option value="{{$select}}" {{ in_array((string)$select, request($name, []))  ?'selected':'' }}>{{$option}}</option>
+        @if(is_array(request($name, [])))
+        <option value="{{$select}}" {{ in_array((string)$select, request($name, [])) ? 'selected':'' }}>{{$option}}</option>
+        @else
+        <option value="{{$select}}" {{ (string)$select === (string)request($name) ? 'selected':'' }}>{{$option}}</option>
+        @endif
     @endforeach
 </select>
