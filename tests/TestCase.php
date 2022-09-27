46466b35c1b1e904d4f098097039533cd62e6c09
Nobuyuki Tabuchi <n.tabuchi@rolo.jp>
2022-09-27 18:37:18 +0900

    feat update database configuration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -41,7 +41,7 @@ class TestCase extends BaseTestCase
         $this->app['config']->set('database.connections.mysql.host', env('MYSQL_HOST', 'localhost'));
         $this->app['config']->set('database.connections.mysql.database', 'laravel_admin_test');
         $this->app['config']->set('database.connections.mysql.username', 'root');
-        $this->app['config']->set('database.connections.mysql.password', '');
+        $this->app['config']->set('database.connections.mysql.password', 'password');
         $this->app['config']->set('app.key', 'AckfSECXIvnK5r28GVIWUAxmbBSjTsmF');
         $this->app['config']->set('filesystems', require __DIR__.'/config/filesystems.php');
         $this->app['config']->set('admin', $adminConfig);
