4e7b7f1f8df9e29741b7d32b5fdf5eac08210e5e
Zushi <t-zushi@st-m.jp>
2024-05-01 16:09:54 +0900

    add span total-row.blade.php

--- resources/views/grid/total-row.blade.php
+++ resources/views/grid/total-row.blade.php
@@ -1,7 +1,7 @@
 <tfoot>
     <tr>
         @foreach($columns as $column)
-            <td>{!! $column !!}</td>
+            <td><span>{!! $column !!}</span></td>
         @endforeach
     </tr>
 </tfoot>
