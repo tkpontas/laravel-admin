141d4ae78a2855b5c215ed9e222b88434231722a
Zushi <t-zushi@st-m.jp>
2024-08-15 17:09:08 +0900

    Update quick-search.blade.php
    データ一覧画面の検索ボタンに「検索」の文字追加
    検索にid=queryを追加することでEnterキー検索を可能にする

--- resources/views/grid/quick-search.blade.php
+++ resources/views/grid/quick-search.blade.php
@@ -1,9 +1,9 @@
 <form action="{!! $action !!}" pjax-container style="display: inline-block;">
     <div class="input-group input-group-sm hidden-xs" style="display: inline-block;">
-        <input type="text" name="{{ $key }}" class="form-control" style="width: 200px;" value="{{ $value }}">
+        <input type="text" id="query" name="{{ $key }}" class="form-control" style="width: 200px;" value="{{ $value }}">
 
         <div class="input-group-btn" style="display: inline-block;">
-            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i> 検索</button>
         </div>
     </div>
 </form>
