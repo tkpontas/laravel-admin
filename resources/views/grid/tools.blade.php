2b1d8238c70fabcb85b9e90413b06b1676d1d527
ex1sudc <ex1sudc@exceedone.co.jp>
2025-03-03 15:36:09 +0700

    fix new btn

--- resources/views/grid/tools.blade.php
+++ resources/views/grid/tools.blade.php
@@ -1,6 +1,6 @@
 <div class="col d-flex align-items-center justify-content-start">
     {!! $grid->renderHeaderTools('left') !!}
 </div>
-<div class="col d-flex justify-content-start flex-nowrap text-nowrap flex-row-reverse">
+<div class="col d-flex justify-content-start flex-nowrap text-nowrap flex-row-reverse p-0">
     {!! $grid->renderHeaderTools('right') !!}
 </div>
