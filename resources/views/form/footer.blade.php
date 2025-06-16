159fc855fe04f62862a251b22c610e18f4fa64b2
vohoangnhat <vohoangnhat@gmail.com>
2025-06-16 15:41:30 +0700

    Fix footer

--- resources/views/form/footer.blade.php
+++ resources/views/form/footer.blade.php
@@ -1,20 +1,23 @@
 <div class="box-footer mt-3 border-top border-light py-3 d-flex" style="background-color: inherit;">
 
-    {{ csrf_field() }}
+    @csrf
 
-    <div class="col-md-{{$width['label']}}">
+    <div class="col-md-{{ $width['label'] }}">
     </div>
 
-    <div class="col-md-{{$width['field']}}">
+    <div class="col-md-{{ $width['field'] }}">
 
         @if(in_array('submit', $buttons))
         <div class="btn-group float-end">
-            <button id="admin-submit" type="submit" class="btn btn-primary">{{ $submitLabel ?? trans('admin.submit') }}</button>
+            <button id="admin-submit" type="submit" class="btn btn-primary">{{ $submitLabel ?? __('admin.submit') }}</button>
         </div>
 
         @foreach($submitRedirects as $redirect)
             <label class="float-end" style="margin: 5px 10px 0 0;">
-                <input type="checkbox" class="after-submit" name="after-save" value="{{ array_get($redirect, 'value') }}" {{ $default_check == array_get($redirect, 'value') ? 'checked' : '' }}> {{ array_get($redirect, 'label') }}
+                <input type="checkbox" class="after-submit" name="after-save"
+                       value="{{ data_get($redirect, 'value') }}"
+                       {{ $default_check == data_get($redirect, 'value') ? 'checked' : '' }}>
+                {{ data_get($redirect, 'label') }}
             </label>
         @endforeach
 
@@ -22,7 +25,7 @@
 
         @if(in_array('reset', $buttons))
         <div class="btn-group float-start">
-            <button type="reset" class="btn btn-warning">{{ trans('admin.reset') }}</button>
+            <button type="reset" class="btn btn-warning">{{ __('admin.reset') }}</button>
         </div>
         @endif
     </div>
